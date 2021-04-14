69e5145c363589e65825187a4f07805618e89bee
MTiberiu <mtdgithub@gmail.com>
2021-04-14 17:09:15 +0300

    Big updates

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
 function sayHi() {
-  return "hello user"
+  return "hello user";
 }
+let h, s, l;
+
+const helloInDifLanguage = [
+  "Ahlan",
+  "Zdrasti",
+  "Hey",
+  "Salut",
+  "Hallo",
+  "Salam!",
+  "Ciao",
+  "Oi",
+  "Hei",
+  "Selam",
+  "Helo",
+  "Yasou",
+];
+let sayHello;
+let helloStyle = document.querySelector("#hello-container");
+function getRandomHello() {
+  sayHello = helloInDifLanguage[Math.floor(Math.random() * 12)];
+}
+
+function displayHello() {
+  getRandomHello();
+  helloStyle.innerHTML = sayHello;
+  helloStyle.style.color = `hsl(${h + 180}, ${s}%, ${l}%)`;
+}
+function randomHSLValues() {
+  h = Math.floor(Math.random() * 360);
+  s = Math.floor(Math.random() * 21 + 80);
+  l = Math.floor(Math.random() * 11 + 55);
+}
+function executeHello() {
+  setTimeout(function () {
+    
+    changeColor();
+    displayHello();
+    console.log(sayHello);
+    executeHello();
+  }, 3000);
+}
+executeHello();
+let selectBody = document.querySelector("body");
+function changeColor() {
+  randomHSLValues();
+
+  helloStyle.style.background = `hsl(${h}, ${s}%, ${l}%)`;
+  helloStyle.style.borderColor = `hsl(${h}, ${s-10}%, ${l-20}%)`;
+}
+
+
+
+
